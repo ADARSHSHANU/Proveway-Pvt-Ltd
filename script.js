7f7a6f667777a1a73286cd19bedff5d5910d6c2c
ADARSH KUMAR CHOUDARY <107990514+ADARSHSHANU@users.noreply.github.com>
2026-01-31 20:19:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const options = document.querySelectorAll('.option');
 const radios = document.querySelectorAll('input[name="bundle"]');
 const totalText = document.querySelector('.total');
 
-// Store selections per bundle
+
 const savedSelections = {};
 
 options.forEach((option, index) => {
     const selects = option.querySelectorAll('select');
 
-    // Save selection when changed
+
     selects.forEach((select, i) => {
         select.addEventListener('change', () => {
             if (!savedSelections[index]) savedSelections[index] = {};
@@ -17,19 +17,19 @@ options.forEach((option, index) => {
     });
 
     option.addEventListener('click', () => {
-        // Remove selected from all
+    
         options.forEach(o => o.classList.remove('selected'));
         radios.forEach(r => r.checked = false);
 
-        // Activate clicked
+
         option.classList.add('selected');
         radios[index].checked = true;
 
-        // Update total price
+
         const price = option.getAttribute('data-price');
         totalText.textContent = "Total : $" + price + " USD";
 
-        // Restore previous selections
+
         if (savedSelections[index]) {
             selects.forEach((select, i) => {
                 if (savedSelections[index][i]) {
